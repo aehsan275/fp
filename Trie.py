3657bed692524cbab1f11c1ce9ebd38acc8bb41b
Abdullah Ehsan <abdullahehsan275@gmail.com>
2025-10-06 18:55:39 -0600

    added encapsulation

--- Trie.py
+++ Trie.py
@@ -1,7 +1,21 @@
 class TrieNode:
     def __init__(self):
-        self.children = [None] * 26
-        self.endOfWord = False
+        self.__children = [None] * 26
+        self.__end_of_word = False
+
+    def get_children(self):
+        return self.__children
+    
+    def set_children(self, index, value):
+        self.__children[index] = value
+
+    def get_end_of_word(self):
+        return self.__end_of_word
+    
+    def set_end_of_word(self, value):
+        self.__end_of_word = value
+
+
 
 class Trie:
 
@@ -18,40 +32,40 @@ class Trie:
         current = self.__root # start at the root
         for character in word: # go through every character in the word
             index = ord(character) - 65 # 65 is the ASCII for 'A', so subtracting gives the index of the character in the alphabet
-            if current.children[index] is None: # if next node does not exist
-                current.children[index] = TrieNode() # initialize next node
-            current = current.children[index] # move to next node
-        current.endOfWord = True # the word has now ended
+            if current.get_children()[index] is None: # if next node does not exist
+                current.set_children(index, TrieNode()) # initialize next node
+            current = current.get_children()[index] # move to next node
+        current.set_end_of_word(True)# the word has now ended
 
     def is_valid_word(self, word):
         current = self.__root
         for character in word:
             index = ord(character) - 65
-            if current.children[index] is None:
+            if current.get_children()[index] is None:
                 return False
-            current = current.children[index]
-        return current.endOfWord
+            current = current.get_children()[index]
+        return current.get_end_of_word()
     
     def get_valid_words(self, prefix):
         start = self.__root
         for character in prefix:
             index = ord(character) - 65
-            if start.children[index] is None:
+            if start.get_children()[index] is None:
                 return []
-            start = start.children[index]
+            start = start.get_children()[index]
         
-        words = self.dfs(start, prefix)
+        words = self.__dfs(start, prefix)
         return words
 
     def __dfs(self, current_node, current_word): # using dfs to traverse through trie to get valid words
         
         words = []
-        if current_node.endOfWord:
+        if current_node.get_end_of_word():
             words.append(current_word)
         
-        for index,child in enumerate(current_node.children):
+        for index,child in enumerate(current_node.get_children()):
             if child is not None:
-                words.extend(self.__dfs(current_node.children[index], current_word + chr(index + 65)))
+                words.extend(self.__dfs(current_node.get_children()[index], current_word + chr(index + 65)))
 
         return words
         
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Trie import Trie
 def main():
     trie = Trie("words.txt")
     print(trie.is_valid_word("AA"))
-    trie.__dfs(1,1)
+    print(trie.get_valid_words("AA"))
 
 
 if __name__ == "__main__":
